2a53757c448507d05cf974adce9610afaa40dd13
Jimmy Klein <james.klein@temple.edu>
2017-03-20 17:15:14 -0400

    Trying to figure out this uri stuff

--- app/src/main/java/edu/temple/tuf21842/rsatext/MainActivity.java
+++ app/src/main/java/edu/temple/tuf21842/rsatext/MainActivity.java
@@ -1,13 +1,20 @@
 package edu.temple.tuf21842.rsatext;
 
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
+    private final String TAG = "MainActivity";
+    ContentResolver cr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        cr = getContentResolver();
+        cr.query(, null, null, null, null);
         setContentView(R.layout.activity_main);
     }
 }
--- app/src/main/java/edu/temple/tuf21842/rsatext/RSAContentProvider.java
+++ app/src/main/java/edu/temple/tuf21842/rsatext/RSAContentProvider.java
@@ -2,10 +2,27 @@ package edu.temple.tuf21842.rsatext;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.database.Cursor;
+import android.database.MatrixCursor;
 import android.net.Uri;
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyProperties;
+import android.util.Log;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.KeyStore;
+import java.security.Signature;
+import java.util.Enumeration;
 
 public class RSAContentProvider extends ContentProvider {
+    private KeyPairGenerator keyPairGenerator;
+    private KeyPair keyPair;
+    private SharedPreferences preferences;
+    private final String TAG = "RSAContentProvider";
+
     public RSAContentProvider() {
     }
 
@@ -30,6 +47,28 @@ public class RSAContentProvider extends ContentProvider {
 
     @Override
     public boolean onCreate() {
+        //SharedPreferences pref = getContext().getSharedPreferences("edu.temple.tuf21842.rsa", Context.MODE_PRIVATE);
+        String privateKey = "";
+        String publicKey = "";
+        try {
+            keyPairGenerator = KeyPairGenerator.getInstance(KeyProperties.KEY_ALGORITHM_RSA);
+            keyPairGenerator.initialize(
+                    new KeyGenParameterSpec.Builder(
+                            "key1",
+                            KeyProperties.PURPOSE_SIGN)
+                            .setDigests(KeyProperties.DIGEST_SHA256, KeyProperties.DIGEST_SHA512)
+                            .setSignaturePaddings(KeyProperties.SIGNATURE_PADDING_RSA_PSS)
+                            .build());
+            keyPair = keyPairGenerator.genKeyPair();
+            KeyStore ks = KeyStore.getInstance("AndroidKeyStore");
+            Log.d(TAG, keyPair.getPrivate().toString());
+            ks.load(null);
+            Enumeration<String> aliases = ks.aliases();
+            Log.d(TAG, aliases.nextElement());
+            //Signature signature = Signature.getInstance("SHA256withRSA/PSS");
+        } catch (Exception e) {
+            Log.d(TAG, e.toString());
+        }
         // TODO: Implement this to initialize your content provider on startup.
         return false;
     }
@@ -38,7 +77,10 @@ public class RSAContentProvider extends ContentProvider {
     public Cursor query(Uri uri, String[] projection, String selection,
                         String[] selectionArgs, String sortOrder) {
         // TODO: Implement this to handle query requests from clients.
-        throw new UnsupportedOperationException("Not yet implemented");
+        //throw new UnsupportedOperationException("Not yet implemented");
+        MatrixCursor matrixCursor = new MatrixCursor(new String[]{"private","public"});
+        matrixCursor.addRow(new Object[]{keyPair.getPrivate().toString(),keyPair.getPublic().toString()});
+        return matrixCursor;
     }
 
     @Override
